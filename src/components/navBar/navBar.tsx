200a0403a49f0924ac5267e12244814fef5083df
Lucas Khristian <lucaasdias@gmail.com>
2022-09-13 17:20:39 -0300

    Reformulação NavBar #2

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,23 +1,47 @@
+import IonIcon from '@reacticons/ionicons';
+
 export function NavBar() {
+
     return (
-        <nav className="flex items-center bg-[#0377AA] p-2">
-            <div className="inline-flex items-center">
-                <img className="px-3 h-9 w-13" src={"assets/pje2.png"}/>
+        <nav className="p-2 bg-[#0377AA] md:flex md:items-center md:justify-between">
+            
+            <div className="flex justify-between items-center">
                 <span className="text-[18px] text-white">
-                Processo Judicial Eletrônico - Simulador
+                    <img className=" h-9 mx-2 inline" src={"assets/pje2.png"}/>
+                    Processo Judicial Eletrônico - Simulador
+                </span>
+                <span className="text-gray-400 cursor-pointer md:hidden block border rounded shadow-lg">
+                    <IonIcon className="text-3xl mx-2" name="menu"></IonIcon>
                 </span>
             </div>
-            <div className="top-nav inline-flex flex-grow w-auto ">
-                <div className="inline-flex flex-row ml-auto text-[14px] text-white">
-                    <a href="#" className="mr-8">Entrar</a>
-                    <a href="#" className="mr-8">Formas de acesso</a>
-                    <a href="#" className="mr-8">Consulta Processual</a>
-                    <a href="#" className="mr-8">Push</a>
-                    <a href="#" className="mr-8">Pré-requisitos</a>
-                    <a href="#" className="mr-8">Manuais</a>
-                    <a href="#" className="mr-8">Fale Conosco</a>
-                </div>
-            </div>
+
+            <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+                <li className=" mx-4 my-6 md:my-0">
+                    <a href="#" className="text-white text-[14px] ">Entrar</a>
+                </li>
+                <li className=" mx-4 my-6 md:my-0">
+                    <a href="#" className="text-white text-[14px]">Formas de acesso</a>
+                </li>
+                <li className=" mx-4 my-6 md:my-0">
+                    <a href="#" className="text-white text-[14px]">Consulta processual</a>
+                </li>
+                <li className=" mx-4 my-6 md:my-0">
+                    <a href="#" className="text-white text-[14px]">Push</a>
+                </li>
+                <li className=" mx-4 my-6 md:my-0">
+                    <a href="#" className="text-white text-[14px]">Pré-requisitos</a>
+                </li>
+                <li className=" mx-4 my-6 md:my-0">
+                    <a href="#" className="text-white text-[14px]">Manuais</a>
+                </li>
+                <li className=" mx-4 my-6 md:my-0">
+                    <a href="#" className=" text-white text-[14px]">Fale conosco</a>
+                </li>
+            </ul>
         </nav>
+
     );
 }
+
+
+
