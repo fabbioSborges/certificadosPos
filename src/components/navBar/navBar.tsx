bd648895cc61bf152e08f3015044ece309b11739
RyanMelo <ryangarciamelo@gmail.com>
2022-09-13 21:11:05 -0300

    menu responsive #2

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -2,20 +2,33 @@ import IonIcon from '@reacticons/ionicons';
 
 export function NavBar() {
 
+    var navMenu = document.getElementById('nav-menu');
+
+    function responsiveMenu() {
+        if(navMenu?.classList.contains('absolute')) {
+            navMenu.classList.remove('absolute')
+        } else {
+            navMenu?.classList.add('absolute')
+        }
+    }
+
     return (
         <nav className="p-2 bg-[#0377AA] md:flex md:items-center md:justify-between">
             
             <div className="flex justify-between items-center">
                 <span className="text-[18px] text-white">
-                    <img className=" h-9 mx-2 inline" src={"assets/pje2.png"}/>
+                    <img className=" h-9 mx-2 inline" src="assets/pje2.png"/>
                     Processo Judicial Eletrônico - Simulador
                 </span>
-                <span className="text-gray-400 cursor-pointer md:hidden block border rounded shadow-lg">
-                    <IonIcon className="text-3xl mx-2" name="menu"></IonIcon>
-                </span>
+                
+                <button onClick={responsiveMenu}>
+                    <span className="text-gray-400 cursor-pointer md:hidden block border rounded shadow-lg">
+                        <IonIcon className="text-3xl mx-2" name="menu"></IonIcon>
+                    </span>
+                </button>
             </div>
 
-            <ul className="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500">
+            <ul id="nav-menu" className="md:flex md:items-center md:static w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-100 transition-all ease-in duration-500">
                 <li className=" mx-4 my-6 md:my-0">
                     <a href="#" className="text-white text-[14px] ">Entrar</a>
                 </li>
