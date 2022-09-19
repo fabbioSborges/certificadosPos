06f36813bacd3cf2a602e61339f8234c85058d57
RyanMelo <ryangarciamelo@gmail.com>
2022-09-18 22:17:46 -0300

    Implementação do menu responsivo #2

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,34 +1,27 @@
 import IonIcon from '@reacticons/ionicons';
+import { useState } from 'react';
 
 export function NavBar() {
 
-    var navMenu = document.getElementById('nav-menu');
-
-    function responsiveMenu() {
-        if(navMenu?.classList.contains('absolute')) {
-            navMenu.classList.remove('absolute')
-        } else {
-            navMenu?.classList.add('absolute')
-        }
-    }
+    const [visibleHamburgermenu, setVisibleHamburgermenu] = useState(false);
 
     return (
         <nav className="p-2 bg-[#0377AA] md:flex md:items-center md:justify-between">
             
             <div className="flex justify-between items-center">
-                <span className="text-[18px] text-white">
-                    <img className=" h-9 mx-2 inline" src="assets/pje2.png"/>
+                <span className="md:text-[18px] text-[14px] text-white">
+                    <img className="md:h-9 h-7 mx-2 inline" src="assets/pje2.png"/>
                     Processo Judicial Eletrônico - Simulador
                 </span>
                 
-                <button onClick={responsiveMenu}>
+                <button onClick={(e) => {visibleHamburgermenu ? setVisibleHamburgermenu(false) : setVisibleHamburgermenu(true)}}>
                     <span className="text-gray-400 cursor-pointer md:hidden block border rounded shadow-lg">
                         <IonIcon className="text-3xl mx-2" name="menu"></IonIcon>
                     </span>
                 </button>
             </div>
 
-            <ul id="nav-menu" className="md:flex md:items-center md:static w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-100 transition-all ease-in duration-500">
+            <ul className={ visibleHamburgermenu ? "md:flex md:items-center md:static md:w-auto md:py-0 py-2.5 md:pl-0" : "md:flex md:items-center md:static md:w-auto md:py-0 py-2.5 md:pl-0 hidden" }>
                 <li className=" mx-4 my-6 md:my-0">
                     <a href="#" className="text-white text-[14px] ">Entrar</a>
                 </li>
--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -2,39 +2,37 @@ import { NavBar } from "components";
 
 export function LoginPage() {
     return (
-        <div className="bg-[#EAF0F3] md:h-screen h-full">
+        <div className="bg-[#EAF0F3] h-screen">
             <header>
                 <NavBar/>
             </header>
 
             <main className='flex flex-col justify-center items-center'>
-                <div className='flex flex-col justify-center items-center mt-28'>
+                <div className='flex flex-col justify-center items-center mt-10 md:mt-28'>
                     <img className='h-[100px] w-[100px]' src="assets/brasaoRep.png" alt="Brasao Republica" />
                     <h2 className='md:text-2xl text-xl text-[#074863] mt-2'>Processo Judicial Eletrônico - 1º Grau - TJPI</h2>
-                    <p className='text-xl text-[#074863] mt-2'>Tribunal de Justiça do Piauí</p>
+                    <p className='text-lg text-[#074863] mt-2'>Tribunal de Justiça do Piauí</p>
                 </div>
 
-                <div className='p-5 mt-5 mb-10 flex flex-col-reverse md:flex-row justify-center items-center bg-white drop-shadow-lg rounded-md'>                  
+                <div className='py-4 px-5 mt-5 flex flex-col-reverse md:flex-row justify-center items-center bg-white drop-shadow-lg rounded-md'>                  
                     <button className='flex flex-row justify-between items-center text-white bg-[#3E92B6] rounded-sm'>    
                         <img className="w-[50px]" src="assets/carteirDigital.png" alt="carteira digital" />
                         <p className='py-2.5 px-7'>CERTIFICADO DIGITAL</p>
                     </button>
 
-                    <div className="flex flex-row md:flex-col items-center justify-center mt-5 mb-5 md:mt-0">
-                        <div className="h-[2px] w-[95px] md:w-[2px] md:h-[80px] bg-[#EAE9E9]"></div>
+                    <div className="flex flex-row md:flex-col items-center justify-center mt-5 md:mt-0">
+                        <div className="h-[2px] w-[95px] md:w-[2px] md:h-[75px] bg-[#EAE9E9]"></div>
                         <span className="mx-4 text-gray-500">ou</span>
-                        <div className="h-[2px] w-[95px] md:w-[2px] md:h-[80px] bg-[#EAE9E9]"></div>
+                        <div className="h-[2px] w-[95px] md:w-[2px] md:h-[75px] bg-[#EAE9E9]"></div>
                     </div>
 
                     <form className="flex flex-col items-end" action="">
-                        <input className="p-2.5 w-64 outline outline-offset-2 outline-1 outline-[#CCCCCC] text-base rounded-sm" type="text" placeholder="CPF / CNPJ"/>
-                        <input className="p-2.5 w-64 outline outline-offset-2 outline-1 outline-[#CCCCCC] mt-5 text-base rounded-sm" type="password" placeholder="Senha"/>
+                        <input className="p-2.5 w-64 outline outline-offset-2 outline-1 outline-[#CCCCCC] text-base rounded-sm focus:ring focus:ring-[#3E92B6]-300" type="text" placeholder="CPF / CNPJ"/>
+                        <input className="p-2.5 w-64 outline outline-offset-2 outline-1 outline-[#CCCCCC] mt-5 text-base rounded-sm focus:ring focus:ring-[#3E92B6]-300" type="password" placeholder="Senha"/>
                         <button className="bg-[#3E92B6] px-5 py-2 text-white mt-5 rounded-md" type="submit">ENTRAR</button>
                     </form>
                 </div>
-
             </main>
-
         </div>
     );
 }
