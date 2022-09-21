cb77c95fce5048dc9f397326f52e3cf61490e153
Lucas Khristian <lucaasdias@gmail.com>
2022-09-21 03:54:10 -0300

    Atualização NavBarDashboard #4

--- src/components/navBarDashboard/navBarDashboard.tsx
+++ src/components/navBarDashboard/navBarDashboard.tsx
@@ -1,31 +1,75 @@
 import IonIcon from '@reacticons/ionicons';
+import { useState } from 'react';
 
 export function NavBarDashboard() {
 
+    const [visibleHamburgermenu, setVisibleHamburgermenu] = useState(false);
+
     return (
-        <nav className="p-2 bg-[#0377AA] md:flex md:items-center md:justify-between">
-            
-            <div className="flex justify-between items-center">
-                <button>
-                    <span className="text-white cursor-pointer rounded ">
-                        <IonIcon className="text-3xl mx-2" name="menu"></IonIcon>
-                    </span>
+        <>
+        <nav className="px-3 bg-[#0377AA] flex items-center justify-between">
+            <div className="flex justify-between items-center ">
+                <button onClick={(e) => {visibleHamburgermenu ? setVisibleHamburgermenu(false) : setVisibleHamburgermenu(true)}}>
+                    <div className={visibleHamburgermenu ? "text-white cursor-pointer py-1.5" : "text-[#0377AA] bg-white cursor-pointer py-1.5"}>
+                        <IonIcon className="text-3xl mx-2 " name={visibleHamburgermenu ? "menu" : "close"}></IonIcon>
+                    </div>
                 </button>
                 
-                <span className="md:text-[18px] text-[14px] text-white">
-                    <img className="md:h-9 h-7 mx-2 inline" src="assets/images/pje2.png"/>
+                <span className="text-[18px]  text-white">
+                    <img className="h-9 mx-2 inline" src="assets/images/pje2.png"/>
                     Painel do Advogado
                 </span>            
             </div>
 
-            <div className="flex justify-between items-center">
+            <div className="flex justify-between items-center my-2">
                 <span className="text-white mx-3">
-                    Horacio Neiva
+                    Nome do Usuario
                 </span>
-                <IonIcon className="text-2xl text-white p-2 rounded-full border" name="person"></IonIcon>
+                <IonIcon className="text-2xl text-white rounded-full bg-[#004F6E]  p-1" name="person"></IonIcon>
+            </div>
+        </nav>
+        <div className = {visibleHamburgermenu ? "hidden" : "bg-white w-[300px] h-screen flex flex-col"}>
+            <div className="flex m-4 border-b border-[#3E92B6]">
+                <span className="inline-flex items-center mx-2">
+                    <IonIcon className="text-[20px] text-[#3E92B6] p-1" name="search"></IonIcon>
+                </span>
+                <input type="text" className="px-1" placeholder="Acesso Rápido"></input>
             </div>
 
+            <div className="">
+                <button className="flex flex-row items-center border-b text-gray-500">
+                    <IonIcon className="text-[26px] py-4 px-6 " name="desktop"></IonIcon>
+                    <p>Painel</p>
+                    <IonIcon className="text-[16px]" name="chevron-forward-outline"></IonIcon>
+                </button>
+
+                <button className="flex flex-row items-center border-b text-gray-500 ">
+                    <IonIcon className="text-[26px] py-4 px-6 " name="folder-open"></IonIcon>
+                    <p>Processo</p>
+                    <IonIcon className="text-[16px]" name="chevron-forward-outline"></IonIcon>
+                </button>
+
+                <button className="flex flex-row items-center border-b text-gray-500 ">
+                    <IonIcon className="text-[26px] py-4 px-6 " name="create"></IonIcon>
+                    <p>Atividades</p>
+                    <IonIcon className="text-[16px]" name="chevron-forward-outline"></IonIcon>
+                </button>
+
+                <button className="flex flex-row items-center border-b text-gray-500 ">
+                    <IonIcon className="text-[26px] py-4 px-6 " name="calendar"></IonIcon>
+                    <p>Audiências e sessões</p>
+                    <IonIcon className="text-[16px]" name="chevron-forward-outline"></IonIcon>
+                </button>
+
+                <button className="flex flex-row items-center border-b text-gray-500 ">
+                    <IonIcon className="text-[26px] py-4 px-6 " name="settings"></IonIcon>
+                    <p>Configuração</p>
+                    <IonIcon className="text-[16px]" name="chevron-forward-outline"></IonIcon>
+                </button>
+
+            </div>
             
-        </nav>
+        </div>
+        </>
     );
 }
